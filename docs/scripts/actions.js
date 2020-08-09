818a5ff23f9d83b564f2784841501f30b64fb6a1
Josh Matthew Talplacido <joshmatthewtalplacido@gmail.com>
2020-08-10 04:54:43 +0800

    ready for deployment

--- docs/scripts/actions.js
+++ docs/scripts/actions.js
@@ -100,7 +100,7 @@ window.onload = function () {
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
       redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      referrerPolicy: 'origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
     return response.json(); // parses JSON response into native JavaScript objects
