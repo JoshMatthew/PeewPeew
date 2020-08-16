1cde7140600d4b4303e2eb096ae5916691c3bf96
joshmatthew <joshmatthewtalplacido@gmail.com>
2020-08-16 19:11:14 +0800

    Edited the loading screen when getting Hereum points

--- docs/scripts/actions.js
+++ docs/scripts/actions.js
@@ -21,6 +21,8 @@ window.onload = function () {
   const params = new URLSearchParams(window.location.search) // gets the acc id of the metamask user
   if (params.has('acc')) {
     window.localStorage.setItem('acc_id', params.get('acc')) // sets the id to the localstorage
+  } else {
+    window.localStorage.setItem('acc_id', '0x2573F143d3674D995C339Af3682Fc4cc29eb7882') // hardcode it for now
   }
 
   if (userPreferences.getItem('controls') === null || userPreferences.getItem('controls') === undefined || userPreferences.getItem('controls') === '') {
@@ -78,24 +80,30 @@ window.onload = function () {
   })
 
   claimHre.addEventListener('click', () => { // Claims the score and send it to the database
-    window.alert("Wait for it to load.")
     const points = window.localStorage.getItem('player_score')
     const acc_id = window.localStorage.getItem('acc_id')
 
-    axios.post('https://hereumapi.herokuapp.com/peewpeew/points_add', { acc_id, points })
-      .then(res => {
-        window.alert('Points sent! Return to Hereum and refresh your points!')
-        exitBtn.click()
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    if (acc_id === 'none') {
+      window.alert("You're not connected to Hereum")
+    } else {
+      isLoading()
+      axios.post('https://hereumapi.herokuapp.com/peewpeew/points_add', { acc_id, points })
+        .then(res => {
+          isLoading()
+          window.alert('Points sent! Return to Hereum and refresh your points!')
+          exitBtn.click()
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   })
 }
 
 // These functions are outside because we can't declare functions inside another function ;
 const restartModal = document.querySelector('.restart-modal')
 const gameDetails = document.querySelector('.game-details')
+const loading = document.querySelector('.loading-screen')
 
 function toggleModal() { // Shows the restart modal or hide it
   restartModal.classList.toggle('hide')
@@ -103,4 +111,9 @@ function toggleModal() { // Shows the restart modal or hide it
 
 function toggleDetails() { // Shows the ingame details such as life, ammo and clips remaining
   gameDetails.classList.toggle('hide')
+}
+
+function isLoading() {
+  loading.classList.toggle('hide')
+  toggleModal()
 }
